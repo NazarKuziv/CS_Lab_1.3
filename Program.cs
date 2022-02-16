4ba6595812661397d20e0d8a0748954e96213704
NazarKuziv <nazar123kuziv@gmail.com>
2022-02-16 17:47:31 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Lab_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, N;
+            double S = 1;
+
+            Console.Write("N = ");
+            N = Convert.ToInt32(Console.ReadLine());
+            k = N;
+
+            while (k <= 19)
+            {
+                S *= (1.0 * k - N) / (1.0 * k + N) + 1;
+                k++;
+            }
+            Console.WriteLine($"S1 = {S}");
+
+            S = 1;
+            k = N;
+            do
+            {
+                S *= (1.0 * k - N) / (1.0 * k + N) + 1;
+                k++;
+            } while (k <= 19);
+            Console.WriteLine($"S2 = {S}");
+
+            S = 1;
+            for (k = N; k <= 19; k++)
+            {
+                S *= (1.0 * k - N) / (1.0 * k + N) + 1;
+            }
+            Console.WriteLine($"S3 = {S}");
+           
+            S = 1;
+            for (k = 19; k >= N; k--)
+            {
+                S *= (1.0 * k - N) / (1.0 * k + N) + 1;
+            }
+            Console.WriteLine($"S3 = {S}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
