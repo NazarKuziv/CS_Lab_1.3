6ccc84b30e00e38d1c29beefe7c32253fd4bb89e
NazarKuziv <nazar123kuziv@gmail.com>
2022-02-23 16:00:54 +0200

    hg

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace CS_Lab_1._3
                 S *= (1.0 * k - N) / (1.0 * k + N) + 1;
                 k++;
             }
-            Console.WriteLine($"S1 = {S}");
+            Console.WriteLine($"S1 = {Math.Round(S, 2)}");
 
             S = 1;
             k = N;
@@ -31,21 +31,21 @@ namespace CS_Lab_1._3
                 S *= (1.0 * k - N) / (1.0 * k + N) + 1;
                 k++;
             } while (k <= 19);
-            Console.WriteLine($"S2 = {S}");
+            Console.WriteLine($"S2 = {Math.Round(S, 2)}");
 
             S = 1;
             for (k = N; k <= 19; k++)
             {
                 S *= (1.0 * k - N) / (1.0 * k + N) + 1;
             }
-            Console.WriteLine($"S3 = {S}");
+            Console.WriteLine($"S3 = {Math.Round(S, 2)}");
            
             S = 1;
             for (k = 19; k >= N; k--)
             {
                 S *= (1.0 * k - N) / (1.0 * k + N) + 1;
             }
-            Console.WriteLine($"S3 = {S}");
+            Console.WriteLine($"S3 = {Math.Round(S, 2)}");
 
             Console.ReadKey();
 
